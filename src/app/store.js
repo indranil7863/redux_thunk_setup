b310f11fd8b595e467adb06bc461193c372e3205
indranil7863 <ibera866@gmail.com>
2026-04-05 22:45:48 +0530

    initialize project

---
+++ src/app/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userDetails from "../features/userDetailsSlice";
+
+export const store = configureStore({
+  reducer: { app: userDetails },
+});
