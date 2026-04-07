a11747ef5a618fc81360f120ac5f7cea2ff743cf
indranil <ibera866@gmail.com>
2026-04-08 00:04:33 +0530

    add create action using redux-thunk

--- src/features/userDetailsSlice.js
+++ src/features/userDetailsSlice.js
@@ -1,4 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+// create action
+export const createUser = createAsyncThunk("createUser", async (data)=>{
+  const response = await fetch("https://69d296cf5043d95be972020d.mockapi.io/APIendpoints", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+})
 
 export const userDetails = createSlice({
   name: "userDetail",
@@ -7,6 +25,19 @@ export const userDetails = createSlice({
     loading: false,
     error: null,
   },
+  extraReducers:{
+    [createUser.pending]: (state)=>{
+      state.loading = true;
+    },
+    [createUser.fulfilled]: (state, action)=>{
+      state.loading = false;
+      state.users.push(action.payload);
+    },
+    [createUser.rejected]: (state, action)=>{
+      state.loading = false;
+      state.users = action.payload;
+    }
+  }
 });
 
 export default userDetails.reducer;
